8bee92d4773cfcbcd357e5457b9b5fd2b248e38b
TimothyYW <>
2023-11-22 19:10:59 +0000

    fix username function and add import

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
-
+import random
+"""
+function getting username including the welcoming message with username that is collected
+"""
 def get_username_name():
-    user_name: input("Enter your name:")
+    user_name = input("Enter your name:")
     print(f"Welcome to the battleship game {user_name}!")
     return user_name
+
+get_username_name()
