996f8d985c96172c50b5a9946d1ef2ec52812324
TimothyYW <>
2023-11-23 21:28:51 +0000

    create display map function

--- run.py
+++ run.py
@@ -1,21 +1,24 @@
 import random
 
-"""
-function getting username including the welcoming message with username that is collected
+""" function getting username including the welcoming message with username that is collected
 """
 def get_username_name():
     user_name = input("Enter your name:")
     print(f"Welcome to the battleship game {user_name}!")
     return user_name
 
-"""
-function to print the board for the game
+""" function to create a map based on size
+
 """
 map_size = 10
 
 def create_battlefield(map_size):
-    
     return [["_"] * map_size for _ in range(map_size)]
 
+""" function to display current state of the map 
+"""
+def display_battlefield(board):
+    for row in board:
+        print(" ".join(row))
 
-print(create_battlefield(map_size))
+
